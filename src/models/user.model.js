63c628341648cfd35d05435a8e587a8e0d5adcff
Bavirisetty Hemanth Manikanta <72919457+hemanth-bavirisetty@users.noreply.github.com>
2024-09-10 21:54:06 +0530

    Models, Bcrypt, Jwt

---
+++ src/models/user.model.js
@@ -0,0 +1,96 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+	{
+		userName: {
+			type: String,
+			required: true,
+			lowercase: true,
+			unique: true,
+			trim: true,
+			index: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			lowercase: true,
+			unique: true,
+			validate: {
+				validator: function (email) {
+					return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+				},
+			},
+			trim: true,
+		},
+		fullName: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+			index: true,
+		},
+		avatar: {
+			type: String,
+			required: true,
+		},
+		coverImage: {
+			type: String,
+		},
+		watchHistory: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Video",
+			},
+		],
+		password: {
+			type: String,
+			required: true,
+		},
+		refreshToken: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+	this.password = await bcrypt.hash(this.password, 10);
+	next();
+});
+
+userSchema.meathods.isPasswordCorrect = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
+
+userSchema.meathods.generateAccessToken = function () {
+	return jwt.sign(
+		{
+			_id: this._id,
+			email: this.email,
+			userName: this.userName,
+			fullName: this.fullName,
+		},
+		process.env.ACCESS_TOKEN_SECRET,
+		{
+			expiresIn: ACCESS_TOKEN_EXPIRY,
+		}
+	);
+};
+
+userSchema.meathods.generateRefreshToken = function () {
+	return jwt.sign(
+		{
+			_id: this._id,
+		},
+		process.env.REFRESH_TOKEN_SECRET,
+		{
+			expiresIn: REFRESH_TOKEN_EXPIRY,
+		}
+	);
+};
+
+export const User = mongoose.model("User", userSchema);
